207545841d449b716b48b04520f6f8d1a693aad2
Eden Xiang <xiang_xuan@qq.com>
2023-02-14 02:10:00 +0800

    :tada: feat: answer for test uploading and ocr function

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from icecream import ic
 import uuid
 
 from flask import Flask, render_template, redirect, url_for
-from flask import request, send_from_directory
+from flask import request
 
 from upload import *
 from process import process_img
@@ -36,27 +36,47 @@ def create():
         path = os.path.join(app.config['UPLOAD_FOLDER'], name)
         os.makedirs(path, exist_ok=True)
         os.makedirs(os.path.join(path, 'ans'), exist_ok=True)
+        os.makedirs(os.path.join(path, 'students'), exist_ok=True)
         return redirect(url_for('detail', test_name=name))
     else:
         return render_template('error.html')
 
 
-@app.route('/test/<test_name>/')
+@app.route('/test/<test_name>/', methods=['GET', 'POST'])
 def detail(test_name):
-    # check if uploaded answer
-    have_ans = False
-    file_list = os.listdir(f'uploads/{test_name}')
-    file_list.remove('ans')
-    print(file_list)
-    if os.listdir(os.path.join(f'uploads/{test_name}', 'ans')):
-        have_ans = True
-    print(have_ans)
-    return render_template('detail.html', file_list=file_list, have_ans=have_ans, test_name=test_name)
+    try:
+        if request.method == 'POST':
+            text = request.form.get("stu_ans")
+            with open(f'uploads/{test_name}/ans/outputs/ans.txt', 'w') as f:
+                f.write(text)
+
+        # check if uploaded answer
+        print(request.method)
+        have_ans = False
+        file_list = os.listdir(f'uploads/{test_name}')
+        file_list.remove('ans')
+        file_list.remove('students')
+        print(file_list)
+        if os.listdir(os.path.join(f'uploads/{test_name}', 'ans')):
+            have_ans = True
+        print(have_ans)
+
+        return render_template('detail.html', file_list=file_list, have_ans=have_ans, test_name=test_name)
+    # prevent user to type a not valid url directly instead of creating a test
+    except:
+        return render_template('error.html')
 
 
-# Upload Image
+# Upload Answer Image
 @app.route('/test/<test_name>/upload_ans/', methods=['POST'])
 def upload_ans(test_name):
+    # Process Answer Image
+    def process(test_name):
+        img_path = os.path.join('uploads', test_name, 'ans')
+        # feed into the model
+        stu_ans = process_img(img_path)
+        return render_template('process.html', test_name=test_name, stu_ans=stu_ans)
+
     print('uploading test ans')
     print(test_name)
     extension = None
@@ -70,24 +90,14 @@ def upload_ans(test_name):
         if file and is_extension_allowed(extension):
             file_name = 'image' + f'.{extension}'
             file.save(os.path.join('uploads', test_name, 'ans', file_name))
-            process(test_name)
+            return process(test_name)
 
     return render_template('error.html')
 
 
-
-# Process Image
-def process(test_name):
-    img_path = os.path.join('uploads', test_name, 'ans')
-    # feed into the model
-    stu_ans = process_img(img_path)
-    return render_template('process.html', stu_ans=stu_ans)
-
-
 # # View ans
-# @app.route('/test/<test_name>/view_ans')
+# @app.route('/test/<test_name>/view-ans')
 # def view_ans(test_name):
-
 
 
 # # Get Image
--- process.py
+++ process.py
@@ -16,24 +16,23 @@ def single_pic_proc(img_path):
 def process_img(ans_path):
     result_dir = os.path.join(ans_path, 'outputs')
     os.makedirs(result_dir, exist_ok=True)
-    ic(ans_path, os.listdir(ans_path))
     img_list = os.listdir(ans_path)
     img_list.remove('outputs')
 
-    for img in img_list:
-        img_path = os.path.join(ans_path, img)
-        result, image_framed = single_pic_proc(img_path)
+    result_txt = os.path.join(result_dir, f'ans.txt')
 
-        name = img.split('.')[1]
-        result_img = os.path.join(result_dir, f'{name}.png')
-        result_txt = os.path.join(result_dir, f'{name}.txt')
+    with open(result_txt, 'w+') as f:
+        for img in img_list:
+            img_path = os.path.join(ans_path, img)
+            result, image_framed = single_pic_proc(img_path)
 
-        Image.fromarray(image_framed).save(result_img)
+            name = img.split('.')[1]
+            result_img = os.path.join(result_dir, f'{name}.png')
 
-        with open(result_txt, 'w+') as f:
+            Image.fromarray(image_framed).save(result_img)
             for key in result:
                 if result[key][1]:
                     f.write(result[key][1] + '\n')
-            f.seek(0)
-            txt = f.read()[:-1]
-        print(txt)
+        f.seek(0)
+        txt = f.read()[:-1]
+    return txt
