c7de4c2780afdec93128f51ced3c3716a2a9ffc5
Eden Xiang <xiang_xuan@qq.com>
2023-02-14 15:04:19 +0800

    :art: refactor: Improve the answer upload workflow

--- app.py
+++ app.py
@@ -70,21 +70,8 @@ def detail(test_name):
 # ####### Answer Part ########
 # ############################
 
-# Upload Answer Image
-@app.route('/tests/<test_name>/upload_ans/', methods=['GET', 'POST'])
-def process_ans(test_name):
-    # Process Answer Image
-    def process(test_name):
-        img_path = os.path.join('uploads', test_name, 'ans')
-        # feed into the model
-        answer = process_img(img_path)
-        return render_template('Answer/process.html', test_name=test_name, answer=answer)
-
-    if request.method == 'GET':
-        with open(f'uploads/{test_name}/ans/outputs/ans.txt', 'r') as f:
-            answer = f.read()
-        return render_template('Answer/process.html', test_name=test_name, answer=answer)
-
+@app.route('/tests/<test_name>/process_ans/', methods=['POST'])
+def process_form(test_name):
     if request.method == 'POST':
         extension = None
         clean_files(100)
@@ -96,13 +83,28 @@ def process_ans(test_name):
         if file and is_extension_allowed(extension):
             file_name = 'image' + f'.{extension}'
             file.save(os.path.join('uploads', test_name, 'ans', file_name))
-            return process(test_name)
+            process_img(os.path.join('uploads', test_name, 'ans'))
+            return redirect(url_for('check_ans', test_name=test_name))
 
     return render_template('error.html')
 
 
+@app.route('/tests/<test_name>/check_ans/', methods=['GET', 'POST'])
+def check_ans(test_name):
+    if request.method == 'GET':
+        with open(f'uploads/{test_name}/ans/outputs/ans.txt', 'r') as f:
+            answer = f.read()
+        return render_template('Answer/check.html', test_name=test_name, answer=answer)
+
+    if request.method == 'POST':
+        text = request.form.get("stu_ans")
+        with open(f'uploads/{test_name}/ans/outputs/ans.txt', 'w') as f:
+            f.write('\n'.join(text.split('\r\n')))
+        return redirect(url_for('detail', test_name=test_name))
+
+
 # View ans
-@app.route('/tests/<test_name>/view_ans/', methods=['GET', 'POST'])
+@app.route('/tests/<test_name>/view_ans/', methods=['GET'])
 def view_ans(test_name):
     with open(f'uploads/{test_name}/ans/outputs/ans.txt', 'r') as f:
         answer = f.read()
@@ -114,7 +116,7 @@ def view_ans(test_name):
 # ################$############
 
 # process student ans
-@app.route('/tests/<test_name>', methods=['POST'])
+@app.route('/tests/<test_name>/process_stu/', methods=['POST'])
 def process_stu_form(test_name):
     if request.method == 'POST':
         extension = None
@@ -165,49 +167,6 @@ def stu_detail(test_name, stu_name):
 # ####### Scoring Part ########
 # #############################
 
-
-# # Get Image
-# @app.route('/uploaded/<filename>')
-# def uploaded(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-
-# View Test
-# @app.route('/view/')
-# def view():
-#     pass
-#
-#
-# # Upload Image
-# @app.route('/upload/', methods=['POST'])
-# def upload():
-#     print('uploading')
-#     img_url = None
-#     extension = None
-#     if request.method == 'POST':
-#         clean_files(100)
-#         file = request.files.get('photo')
-#
-#         if '.' in file.filename:
-#             extension = file.filename.rsplit('.', 1)[1]
-#
-#         if file and is_extension_allowed(extension):
-#             file_name = str(uuid.uuid1()) + f'.{extension}'
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
-#             redirect(url_for('uploaded', filename=file_name))
-#
-#     return render_template('error.html')
-#
-#
-#
-# # Process Image
-# @app.route('/process/<filename>')
-# def process(img_url):
-#     img_path = os.path.join('uploads', img_url.split('/')[-1])
-#     # feed into the model
-#     stu_ans = process_img(img_path)
-#     return render_template('process.html', img_url=img_url, stu_ans=stu_ans)
-#
 #
 # # Show Result
 # @app.route('/result/', methods=['POST'])
