f0ead0ae7beb41eef5fd4749109c60e61b5f6e39
Eden Xiang <xiang_xuan@qq.com>
2023-02-15 17:50:21 +0800

    :sparkles: feat: zip test file function for sending to user

--- process.py
+++ process.py
@@ -5,15 +5,16 @@ from ocr import ocr
 import time
 import numpy as np
 from PIL import Image
+import zipfile
 
 
-def single_pic_proc(img_path):
+def single_pic_proc(img_path, handwriting=True):
     image = np.array(Image.open(img_path).convert('RGB'))
-    result, image_framed = ocr(image)
+    result, image_framed = ocr(image, handwriting)
     return result, image_framed
 
 
-def process_img(ans_path):
+def process_img(ans_path, handwriting=True):
     result_dir = os.path.join(ans_path, 'outputs')
     os.makedirs(result_dir, exist_ok=True)
     img_list = os.listdir(ans_path)
@@ -25,7 +26,7 @@ def process_img(ans_path):
         lines = []
         for img in img_list:
             img_path = os.path.join(ans_path, img)
-            result, image_framed = single_pic_proc(img_path)
+            result, image_framed = single_pic_proc(img_path, handwriting)
 
             name = img.split('.')[1]
             result_img = os.path.join(result_dir, f'{name}.png')
@@ -37,3 +38,18 @@ def process_img(ans_path):
         txt = '\n'.join(lines)
         f.write(txt)
     return txt
+
+
+def zip_test(test_name):
+    test_path = f'./uploads/{test_name}'
+
+    with zipfile.ZipFile(f'{test_name}.zip', 'w', zipfile.ZIP_DEFLATED) as f:
+        for path, dirnames, filenames in os.walk(test_path):
+            fpath = path.replace(test_path, '')
+
+            for filename in filenames:
+                f.write(os.path.join(path, filename), os.path.join(fpath, filename))
+
+
+if __name__ == '__main__':
+    zip_test('juju')
